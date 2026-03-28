07b44ef8723e118f90b21fcfcd11a7fc9c550f2b
PC20Admin <pc20admin@pishi20AdminnoMacBook-Air.local>
2026-03-28 10:57:09 +0900

    Add Pixel Survivor Test version to Selected Works

--- cheat_survivor.js
+++ cheat_survivor.js
@@ -111,7 +111,15 @@ function init(isNextStage = false) {
     isVictory = false;
     bossProjectiles = [];
     robotBProjectiles = [];
-    player.inventory = [];
+    // Temporary script to give items for testing
+    player.inventory = [
+        'knife', 'knife', 'knife', 'knife', 'knife', // 5 knives
+        'boots', 'boots', 'boots', 'boots', // 4 boots
+        'robota', 'robota', 'robota', 'robota', 'robota', // 5 robot a
+        'robotb', 'robotb', 'robotb', 'robotb', 'robotb' // 5 robot b
+    ];
+    xp = 100; // Trigger level up instantly
+    checkLevelUp();
 
     // Spawn 4 initial gems at random locations
     for (let i = 0; i < 4; i++) {
